79f94faaff78ae5e03b6676740e1de53e09b07f6
Nbiba Bedis <bedisnbiba@gmail.com>
2020-07-11 10:13:01 +0100

    mirror

---
+++ mun/src/main.rs
@@ -0,0 +1,12 @@
+use mun::{run_with_args, ExitStatus};
+
+/// Main entry point for the `mun` executable.
+fn main() -> Result<(), anyhow::Error> {
+    pretty_env_logger::try_init()?;
+    let status = run_with_args(std::env::args_os()).unwrap();
+    match status {
+        ExitStatus::Success => {}
+        ExitStatus::Error => std::process::exit(1),
+    };
+    Ok(())
+}
